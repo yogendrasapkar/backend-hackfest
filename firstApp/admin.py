844fe0680c12f815b376e4da890a85811e354ebf
Himanch Chowdary <himanchchowdary@gmail.com>
2022-01-16 21:11:37 +0530

    update: admin panel

--- firstApp/admin.py
+++ firstApp/admin.py
@@ -1,10 +1,10 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
-from .models import medicalsummary, prescription,patientInfo,planCare,dignosticsresults
+from .models import medicalsummary, prescription, patientInfo, planCare, dignosticsresults, problemList
 
 # Register your models here.
 admin.site.register(medicalsummary)
-
+admin.site.register(problemList)
 admin.site.register(prescription)
 admin.site.register(patientInfo)
 admin.site.register(planCare)
